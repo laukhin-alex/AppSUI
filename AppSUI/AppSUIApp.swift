bd4f62ebc337fe2672c69458b90213ed1aa405d5
laukhin-alex <alex.laukhin@yandex.ru>
2022-04-19 15:25:16 +0300

    Initial Commit

---
+++ AppSUI/AppSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSUIApp.swift
+//  AppSUI
+//
+//  Created by Александр on 19.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
